474883e9336f7735d26aabe4817003276b9a72de
drrkkm <61612701+drrkkm@users.noreply.github.com>
2023-02-21 18:13:22 +0000

    merge sort

--- task_03/src/sort.hpp
+++ task_03/src/sort.hpp
@@ -2,4 +2,50 @@
 
 #include <vector>
 
-std::vector<int> Sort(const std::vector<int>& data);
+template <typename T>
+void Merge(std::vector<T> &data, int p, int q, int r){
+  std::vector <T> aboba;
+  int i = p, j = q;
+  while (i < q && j < r){
+   if (data[i] <= data[j]){
+    aboba.push_back(data[i]);
+    i++;
+   }
+   else if (data[i] > data[j]){
+    aboba.push_back(data[j]);
+    j++;
+   }
+  }
+  while(i < q){
+    aboba.push_back(data[i]);
+    i++;
+  }
+  while(j < r){
+    aboba.push_back(data[j]);
+    j++;
+  }
+  for (int k = 0; k < aboba.size(); k++){
+    data[p +k] = aboba[k];
+  }
+}
+
+template <typename T>
+void MergeVector(std::vector<T> &data, int r, int p){
+  if (p - r <= 1) {
+    return;
+  }
+  int q = (p + r) / 2;
+  MergeVector(data, r, q);
+  MergeVector(data, q, p);
+  Merge(data, r, q, p);
+}
+
+
+
+
+template <typename T>
+std::vector<T> Sort(const std::vector<T>& data) {
+  std::vector<T> result = data;
+    MergeVector(result, 0, result.size());
+  return result;
+}

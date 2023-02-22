0ed9f7fbbd1cce3fa042252c962a4ac1d7ddf8b9
drrkkm <61612701+drrkkm@users.noreply.github.com>
2023-02-22 17:38:42 +0000

    heap

--- task_02/src/heap.hpp
+++ task_02/src/heap.hpp
@@ -2,15 +2,69 @@
 
 #include <cstddef>
 #include <functional>
-#include <queue>
 #include <vector>
+#include <stdexcept>
 
+template <typename T>
 class MinHeap {
  public:
-  void Push(int value);
-  int Pop();
+  void Push(T value);
+  T Pop();
   size_t Size();
+  void ShiftDown(int index);
+  void ShiftUp(int index);
 
  private:
-  std::priority_queue<int, std::vector<int>, std::greater<int>> data_;
+  std::vector<T> data_;
 };
+
+
+template <typename T>
+void MinHeap<T>::Push(T value){
+   data_.push_back(value); 
+   ShiftUp(data_.size() - 1);
+}
+
+
+template <typename T>
+void MinHeap<T>::ShiftUp(int index){
+  if (index > 0 && data_[index] < data_[index / 2]) {
+    std::swap(data_[index], data_[index / 2]);
+    ShiftUp(index / 2);
+  }
+  else return;
+}
+
+
+template <typename T>
+void MinHeap<T>::ShiftDown(int index){
+  if (2 * index + 1 > data_.size() - 1 || index > data_.size()) return;
+  else{
+    if (data_[index] == data_[2 * index + 1] && data_[2 * index + 1] == data_[2 * index + 2]) return;
+    else if (data_[index] > data_[2 * index + 1] && data_[2 * index + 1] <= data_[2 * index + 2]){
+      std::swap(data_[2 * index + 1], data_[index]);
+      ShiftDown(2 * index + 1);
+    }
+    else if (data_[index] > data_[2 * index + 2] && data_[2 * index + 1] > data_[2 * index + 2]){
+      std::swap(data_[2 * index + 2], data_[index]);
+      ShiftDown(2 * index + 2);
+    }
+  }
+}
+
+
+template <typename T>
+T MinHeap<T>::Pop() {
+  if (!data_.size()) throw std::out_of_range("Empty heap");
+  T result = data_[0];
+  if (data_.size() > 0){
+    data_[0] = data_.back();
+    data_.pop_back();
+    ShiftDown(0);
+  }
+  return result;
+}
+
+
+template <typename T>
+size_t MinHeap<T>::Size() { return data_.size(); }
